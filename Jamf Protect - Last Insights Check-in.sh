451dfcea9dd4c3fdf7e91d83303b07cc73577230
Robert Schroeder <robjschroeder@gmail.com>
2021-12-26 22:23:16 -0700

    Create file

--- Jamf Protect - Last Insights Check-in.sh
+++ Jamf Protect - Last Insights Check-in.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+## Displays the date and time of the last Insights check-in for Jamf Protect ##
+
 #Jamf Protect Location
 jamfProtectBinaryLocation="/usr/local/bin/protectctl"
 
 if [ -f "$jamfProtectBinaryLocation" ]; then
-    jamfProtectInfoInsightsSync=$("$jamfProtectBinaryLocation" info | sed -n '5 p' |  awk -F 'Last Insights:' '{print $2}' | xargs)
+    jamfProtectInfoInsightsSync=$("$jamfProtectBinaryLocation" info | awk -F 'Last Insights:' '{print $2}' | xargs)
 else
-	jamfProtectInfoInsightsSync="Does not exist"
+	jamfProtectInfoInsightsSync="Protect binary not found"
 fi
 
-echo "jamfProtectInfoInsightsSync"
 echo "<result>$jamfProtectInfoInsightsSync</result>"
