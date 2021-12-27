a2722139ee10d9a41555c8be9b1506164078cf73
Robert Schroeder <robjschroeder@gmail.com>
2021-12-26 22:26:25 -0700

    Update Jamf Protect - Plan ID.sh

--- Jamf Protect - Plan ID.sh
+++ Jamf Protect - Plan ID.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+## Displays the Plan ID specified by Jamf Protect ##
+
 #Jamf Protect Location
 jamfProtectBinaryLocation="/usr/local/bin/protectctl"
 
 if [ -f "$jamfProtectBinaryLocation" ]; then
-    jamfProtectPlanID=$("$jamfProtectBinaryLocation" info | sed -n '1 p' |  awk -F 'Plan ID: ' '{print $2}' | xargs)
+    jamfProtectPlanID=$("$jamfProtectBinaryLocation" info | awk -F 'Plan ID: ' '{print $2}' | xargs)
 else
-	jamfProtectPlanID="Does not exist"
+	jamfProtectPlanID="Protect binary not found"
 fi
 
-echo "jamfProtectPlanID"
 echo "<result>$jamfProtectPlanID</result>"
