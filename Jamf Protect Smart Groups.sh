590045ee42f006b45243f15c349131e6d7771fdc
Robert Schroeder <robjschroeder@gmail.com>
2021-12-26 22:27:20 -0700

    Update Jamf Protect Smart Groups.sh

--- Jamf Protect Smart Groups.sh
+++ Jamf Protect Smart Groups.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## List containing all smart groups scoped by Jamf Protect ##
+
 SMARTGROUPS_DIR=/Library/Application\ Support/JamfProtect/groups
 if [ -d "$SMARTGROUPS_DIR" ]; then
 	SMART_GROUPS=`/bin/ls "$SMARTGROUPS_DIR" | tr '\n' ','`
