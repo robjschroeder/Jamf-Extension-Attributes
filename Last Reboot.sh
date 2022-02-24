624d8f8cb633d62c2fd83f7ab55b8d642f5870fa
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 20:14:11 -0700

    Update Last Reboot.sh

--- Last Reboot.sh
+++ Last Reboot.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Get boottime in epoch time, convert to Jamf Pro formatted time and make an extension attribute
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
 
 bootTime=$(sysctl kern.boottime | awk '{print $5}' | tr -d ,)
 formattedTime=$(date -jf %s $bootTime "+%F %T")
-echo "<result>$formattedTime</result>"
+echo "<result>${formattedTime}</result>"
+
+exit 0
