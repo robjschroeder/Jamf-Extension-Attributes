972d56942773612b484fcfed2a6bde7b7dbdadce
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 19:47:41 -0700

    Update ARD Field 1.sh

--- ARD Field 1.sh
+++ ARD Field 1.sh
@@ -10,3 +10,5 @@
 if [ -f "/Library/Preferences/com.apple.RemoteDesktop.plist" ]; then
 echo "<result>`/usr/bin/defaults read /Library/Preferences/com.apple.RemoteDesktop Text1`</result>"
 fi
+
+exit 0
