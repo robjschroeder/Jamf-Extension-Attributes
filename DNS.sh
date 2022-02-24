62351263c6d218a865464b8f8d716f93827ed5c4
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 19:59:18 -0700

    Update DNS.sh

--- DNS.sh
+++ DNS.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+# Reports the current DNS server
+# on the macOS computer. 
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
 
 # Get DNS entries from SCUTIL
 dns=$(/usr/sbin/scutil --dns | awk '/scoped queries/,/END/{print}' | awk '/nameserver/{print $NF}' )
 # Echo result to EA output
-echo "<result>$dns</result>"
+echo "<result>${dns}</result>"
+
+exit 0
