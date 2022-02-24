bcc691437f85550fa945af8bec740b63ea71397f
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 19:52:14 -0700

    Update Adobe Update Helper Launch Daemon.sh

--- Adobe Update Helper Launch Daemon.sh
+++ Adobe Update Helper Launch Daemon.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-# Reports on the existence of the Adobe Update Helper Launch Daemon
+# Reports on the existence of the 
+# Adobe Update Helper Launch Daemon
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
 
 if [ -e /Library/LaunchAgents/com.adobe.ARMDCHelper* ]; then
   echo "<result>True</result>"
 else
   echo "<result>False</result>"
 fi
+
+exit 0
