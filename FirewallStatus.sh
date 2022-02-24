8672a4afbcb129b56334c85ab7985e435906c032
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 20:04:50 -0700

    Update FirewallStatus.sh

--- FirewallStatus.sh
+++ FirewallStatus.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Reports the status of the Firewall
+# Possible globalstate values:
+# 0 = Off
+# 1 = On for specific services
+# 2 = On for essential services
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
+
 fwstate=$(defaults read /Library/Preferences/com.apple.alf globalstate)
 if [[  $fwstate != "0" ]]; then 
 	echo "Firewall Global State is On"
@@ -8,3 +17,5 @@ else
 	echo "Firewall Global State is Off"
 	echo "<result>Off</result>"
 fi
+
+exit 0
