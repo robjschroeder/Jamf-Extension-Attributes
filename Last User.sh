5facfddf5ef4718d06a9a574604c0637a7e684a4
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 20:16:07 -0700

    Update Last User.sh

--- Last User.sh
+++ Last User.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
-lastUser=`/usr/bin/last -1 -t console | awk '{print $1}'`
+#!/bin/bash
 
-if [ $lastUser == "wtmp" ]; then
+# Report the last logged in user
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
+
+lastUser=$(/usr/bin/last -1 -t console | awk '{print $1}')
+
+if [ ${lastUser} == "wtmp" ]; then
 echo "<result>No logins</result>"
 else
-echo "<result>$lastUser</result>"
+echo "<result>${lastUser}</result>"
 fi
+
+exit 0
