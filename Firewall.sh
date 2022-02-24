4bb8f45774e238967a438f870ff35816f739cc17
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 20:09:24 -0700

    Update Firewall.sh

--- Firewall.sh
+++ Firewall.sh
@@ -2,19 +2,21 @@
 
 # Reports current firewall status
 
-OS=`/usr/bin/sw_vers -productVersion | /usr/bin/colrm 5`
-if [[ "$OS" < "10.5" ]]; then		
-result=`/usr/bin/defaults read /Library/Preferences/com.apple.sharing.firewall state`
-if [ "$result" == "YES" ]; then
-	echo "<result>On</result>"
-elif [ "$result" == "NO" ]; then
+OS=$(/usr/bin/sw_vers -productVersion | /usr/bin/colrm 5)
+if [[ "${OS}" < "10.5" ]]; then		
+	result=$(/usr/bin/defaults read /Library/Preferences/com.apple.sharing.firewall state)
+	if [ "${result}" == "YES" ]; then
+		echo "<result>On</result>"
+	elif [ "${result}" == "NO" ]; then
 	echo "<result>Off</result>"
-fi
+	fi
 else
-result=`/usr/bin/defaults read /Library/Preferences/com.apple.alf globalstate`
-if [ "$result" == "0" ]; then
-	echo "<result>Off</result>"
-else
-	echo "<result>On</result>"
+result=$(/usr/bin/defaults read /Library/Preferences/com.apple.alf globalstate)
+	if [ "${result}" == "0" ]; then
+		echo "<result>Off</result>"
+	else
+		echo "<result>On</result>"
+	fi
 fi
-fi
+
+exit 0
