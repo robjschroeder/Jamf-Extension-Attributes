7506d8f016967edfcb4b0ed8551fc5e5eb5ebb3c
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 20:17:24 -0700

    Update get-Timezone.sh

--- get-Timezone.sh
+++ get-Timezone.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # Reports computer's current timezone
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
+
 timezone=$(systemsetup -gettimezone | cut -b 12-)
 
-echo "<result>$timezone</result>"
+echo "<result>${timezone}</result>"
+
+exit 0
