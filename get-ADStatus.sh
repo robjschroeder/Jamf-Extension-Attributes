b34c9315dc3c031b72fc4dec62c736475da67a08
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 20:21:31 -0700

    Update get-ADStatus.sh

--- get-ADStatus.sh
+++ get-ADStatus.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # Reports AD Status
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
 
 ad_domain_name=$(dsconfigad -show | grep "Active Directory Domain" | cut -d= -f2 | awk '{print $1}')
 
-echo "<result>$ad_domain_name</result>"
+echo "<result>${ad_domain_name}</result>"
+
+exit 0
