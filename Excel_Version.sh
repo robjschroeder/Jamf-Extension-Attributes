dc542857625cf349b0a7c5343df09d0f448d9714
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 20:12:38 -0700

    Update Excel_Version.sh

--- Excel_Version.sh
+++ Excel_Version.sh
@@ -1,8 +1,13 @@
-<?xml version="1.0" encoding="UTF-8"?><extensionAttribute>
-#!/bin/sh
+#!/bin/bash
+
+# Reports the current version of 
+# Microsoft Excel
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
 if [ -d /Applications/Microsoft\ Excel.app ]; then
-    AppVersion=`/usr/bin/defaults read /Applications/Microsoft\ Excel.app/Contents/Info.plist CFBundleShortVersionString`
-    echo "<result>$AppVersion</result>"
+    AppVersion=$(/usr/bin/defaults read /Applications/Microsoft\ Excel.app/Contents/Info.plist CFBundleShortVersionString)
+    echo "<result>${AppVersion}</result>"
 else
     echo "<result>Not installed</result>"
 fi
