5b11f5de74a42ccfbad653b718379ab643b8b5fc
Robert Schroeder <robjschroeder@gmail.com>
2022-02-26 09:21:05 -0700

    Update EFI Password Set.sh

--- EFI Password Set.sh
+++ EFI Password Set.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # Reports if EFI Password is currently set on computer
+#
+# Updated: 2.26.2022 @ Robjschroeder
 
 firmwarePass="$(firmwarepasswd -check)";
 
 if [ "$firmwarePass" == "Password Enabled: Yes" ]; then echo "<result>Set</result>";
 else echo "<result>Not Set</result>"
 fi
+
+exit
