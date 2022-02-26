7bd479ab29605eac223791f070434a91d8f40b4d
Robert Schroeder <robjschroeder@gmail.com>
2022-02-26 09:13:29 -0700

    Update Audit List.sh

--- Audit List.sh
+++ Audit List.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # Security Reporting - List Risks
+# From @gocardless - Updated for Big Sur
+# https://github.com/gocardless/CIS-for-macOS-BigSur-CP
+#
+# Updated: 2.23.2022 @ Robjschroeder
+
+# Security Reporting - List Risks
 
 auditfile=/Library/Application\ Support/SecurityScoring/org_audit
 echo "<result>$(cat "$auditfile")</result>"
