6b3217671f72073be7dd28393ed0cb9c5089365b
Robert Schroeder <robjschroeder@gmail.com>
2022-02-26 09:14:49 -0700

    Update Audit Risk Count.sh

--- Audit Risk Count.sh
+++ Audit Risk Count.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Security Reporting - Count Risk
+# Security Reporting - Count Risks
+# From @gocardless - Updated for Big Sur
+# https://github.com/gocardless/CIS-for-macOS-BigSur-CP
+#
+# Updated: 2.26.2022 @ Robjschroeder
+
+# Security Reporting - Count Risks
 
 auditfile=/Library/Application\ Support/SecurityScoring/org_audit
 echo "<result>$(cat "$auditfile" | grep "*" | wc -l | tr -d '[:space:]')</result>"
