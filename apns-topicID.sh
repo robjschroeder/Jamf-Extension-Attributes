487ee8d4245ee626314433ef802953aa7685358a
Robert Schroeder <robjschroeder@gmail.com>
2022-03-26 21:55:10 -0700

    Update apns-topicID.sh

--- apns-topicID.sh
+++ apns-topicID.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-
+# Reports the Topic ID (serial of the APNS cert) being used on the MDM profile
+#
+# Updated: 3.26.2022 @ Robjschroeder
+#
 
 topicID=`/usr/sbin/system_profiler SPConfigurationProfileDataType | awk '/Topic/{ print $NF }' | sed 's/[";]//g'`
 
@@ -9,3 +12,5 @@ if [ $topicID == "" ]; then
 else
 	echo "<result>$topicID</result>"
 fi
+
+exit 0
