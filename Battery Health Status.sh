5d3e55a9b578096a3a336881a2a0dbd6f9d4f97c
Robert Schroeder <robjschroeder@gmail.com>
2022-03-30 09:52:21 -0700

    Update Battery Health Status.sh

--- Battery Health Status.sh
+++ Battery Health Status.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-# Reports the Permanent Failure Status
+# Reports the Battery Health Status
 # of Mac battery. 
 #
-# Updated: 2.23.2022 @ Robjschroeder
+# Created: 2.23.2022 @ Robjschroeder
+#
+# Updated 3.30.2022 @ Robert Schroeder
+# Now using system_profiler to get the battery condition.
+# Possible results are: 
+#
+# Normal
+# Replace Soon
+# Replace Now
+# Service Battery
+# Not A MacBook
 #
 
-result=$(ioreg -r -c "AppleSmartBattery" | grep "PermanentFailureStatus" | awk '{print $3}' | sed s/\"//g)
+# Determine model of computer
+model=$(system_profiler SPHardwareDataType | grep "Model Name:" | cut -d ' ' -f 9)
 
-if [ "${result}" == "1" ]; then
-result="Failure"
-elif [ "${result}" == "0" ]; then
-result="OK"
+# Get Battery Health status if computer is a MacBook
+if [[ "${model}" =~ "Book" ]]; then
+	#Determine battery health status
+	result=$(system_profiler SPPowerDataType | grep "Condition" | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')
+	echo "<result>${result}</result>"
+else
+	echo "<result>Not A MacBook</result>"
 fi
-
-echo "<result>${result}</result>"
-
-exit 0
