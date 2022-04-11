d8d0d22b5c86ecb34d82cf0c02b496611028c64e
Robert Schroeder <robjschroeder@gmail.com>
2022-04-11 10:54:44 -0700

    Update LastPasswordChange.sh

--- LastPasswordChange.sh
+++ LastPasswordChange.sh
@@ -10,8 +10,12 @@
 # Grab the last logged in User
 lastLoggedInUser=$( defaults read /Library/Preferences/com.apple.loginwindow lastUserName )
 # Get the password change date of that User 
-lastPWChange="$(date -r "$(dscl . -readpl "/Users/$lastLoggedInUser" accountPolicyData passwordLastSetTime | awk -F ' |[.]' '{ print $2; exit }')")"
+# lastPWChange="$(date -r "$(dscl . -readpl "/Users/$lastLoggedInUser" accountPolicyData passwordLastSetTime | awk -F ' |[.]' '{ print $2; exit }')")"
+lastPWChange=$( dscl . -readpl /Users/$lastLoggedInUser accountPolicyData passwordLastSetTime | awk '{print $2}' | sed -n "s/\([0-9]*\).*/\1/p" )
+#Format the time so we can use it as a Date/Time type in Jamf Pro EA
+formattedTime=$(date -jf %s $lastPWChange "+%F %T")
 
-echo "<result>$lastPWChange</result>"
+
+echo "<result>$formattedTime</result>"
 
 exit 0
