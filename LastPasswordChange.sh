610b0d7538f28539e8aa4d3ea3dac1fd02875f97
Robert Schroeder <robjschroeder@gmail.com>
2022-04-11 10:52:29 -0700

    Update LastPasswordChange.sh

--- LastPasswordChange.sh
+++ LastPasswordChange.sh
@@ -3,17 +3,15 @@
 # Extension attribute to grab the last password
 # change for the last logged on user
 #
-# Changed script to use dscl . -readpl
+# Changed script to use dscl . -readpl (suggestion from Pico -- MadAdmins)
 
 # Created 4.11.2022 @robjschroeder
 
 # Grab the last logged in User
 lastLoggedInUser=$( defaults read /Library/Preferences/com.apple.loginwindow lastUserName )
-# Get the password change date of that User
-lastPWChange=$( dscl . -readpl /Users/$lastLoggedInUser accountPolicyData passwordLastSetTime | awk '{print $2}' | sed -n "s/\([0-9]*\).*/\1/p" )
-# Change Time format to readable time
-formattedTime=$(date -jf %s $lastPWChange "+%F %T")
+# Get the password change date of that User 
+lastPWChange="$(date -r "$(dscl . -readpl "/Users/$lastLoggedInUser" accountPolicyData passwordLastSetTime | awk -F ' |[.]' '{ print $2; exit }')")"
 
-echo "<result>$formattedTime</result>"
+echo "<result>$lastPWChange</result>"
 
 exit 0
