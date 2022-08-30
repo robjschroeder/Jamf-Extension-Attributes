a3109812a195c7a21897a8b7e86b0b4fe2a8569c
Robert Schroeder <robjschroeder@gmail.com>
2022-08-30 15:10:22 -0700

    Update OS Upgrade Date.sh

--- OS Upgrade Date.sh
+++ OS Upgrade Date.sh
@@ -8,8 +8,10 @@
 
 # Evaluate the status in shell output
 eval $(/usr/bin/stat -s /)
+
 # Convert st_birthtime from epoch to a formatted time
 formattedTime=$(/bin/date -jf %s "$st_birthtime" "+%F %T")
+
 # Print the results
 echo "<result>${formattedTime}</result>"
 
