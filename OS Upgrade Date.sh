2da85ea4591da169df79b11391d444556f958a3d
Robert Schroeder <robjschroeder@gmail.com>
2022-08-30 15:08:45 -0700

    Update OS Upgrade Date.sh

--- OS Upgrade Date.sh
+++ OS Upgrade Date.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Returns the last OS update/upgrade date of macOS
+#
+# Created 08.30.2022 @robjschroeder
+
+##################################################
+
 # Evaluate the status in shell output
 eval $(/usr/bin/stat -s /)
 # Convert st_birthtime from epoch to a formatted time
-result=$(/bin/date -jf %s "$st_birthtime" "+%F %T")
+formattedTime=$(/bin/date -jf %s "$st_birthtime" "+%F %T")
 # Print the results
-echo "<result>$result</result>"
+echo "<result>${formattedTime}</result>"
 
 exit 0
