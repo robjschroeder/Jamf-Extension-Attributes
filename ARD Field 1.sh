880ea062761a030326bfea40be47f1018c2fb81d
Robert Schroeder <robjschroeder@gmail.com>
2022-11-21 10:20:50 -0700

    Update ARD Field 1.sh

--- ARD Field 1.sh
+++ ARD Field 1.sh
@@ -3,12 +3,20 @@
 # Return the value set in Text1 field 
 # of the Remote Desktop plist
 #
-# Updated: 2.23.2022 @ Robjschroeder
+# Updated: 11.21.2022 @ Robjschroeder
 #
+
 # Reports the value stored in Apple Remote Desktop Field 1
 
-if [ -f "/Library/Preferences/com.apple.RemoteDesktop.plist" ]; then
-echo "<result>`/usr/bin/defaults read /Library/Preferences/com.apple.RemoteDesktop Text1`</result>"
+path="/Library/Preferences"
+domainPlist="com.apple.RemoteDesktop.plist"
+key="Text1"
+defaultsRead="/usr/bin/defaults read $path/$domainPlist $key"
+
+if ($defaultsRead) > /dev/null 2>&1; then
+	RESULT=$($defaultsRead)
+else
+	RESULT="Field does not exist"
 fi
 
-exit 0
+echo "<result>$RESULT</result>"
